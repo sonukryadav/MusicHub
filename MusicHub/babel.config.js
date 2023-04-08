b43e7ff13f2fe800903f635ab850e7175994e252
sonukryadav <sonukryadav56@gmail.com>
2023-04-08 14:50:47 +0530

    Reanimated setup

--- MusicHub/babel.config.js
+++ MusicHub/babel.config.js
@@ -5,6 +5,6 @@ module.exports = {
         "moduleName": "@env",
         "path": ".env",
       }],
-      // ['react-native-reanimated/plugin'],
+      'react-native-reanimated/plugin',
     ],
 };
