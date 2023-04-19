ee4cecceccc620654924967735c5d3eab441a00a
sonukryadav <sonukryadav56@gmail.com>
2023-04-19 18:42:08 +0530

    Separated the component from FlatList

--- MusicHub/Components/Screens2/LocalAudioFiles.jsx
+++ MusicHub/Components/Screens2/LocalAudioFiles.jsx
@@ -35,38 +35,42 @@ export default function LocalAudioFiles() {
         navigation.navigate("singleaudio", {title : item.title, url : item.url, index1 : index});
     }
 
+
+
+    const SongBlock = ({item, index}) => (
+        <>
+            <View style={styles.v2}>
+                <View style={styles.v3}>
+                    <TouchableOpacity onPress={() => singleBlock(item, index)}>
+                        <View style={styles.v3i}>
+                            <View style={styles.v4}>
+                                <Fontisto name={"applemusic"} size={60} color={"black"} />
+                            </View>
+                            <View style={styles.v5}>
+                                <Text style={styles.t1}>{item.title.length > 35 ? item.title.substring(0, 35) + "..." : item.title}</Text>
+                                <Text style={styles.t2}>{"Artist name"}</Text>
+                                <Text style={styles.t3}>{"2:15"}</Text>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.v6}>
+                    <TouchableOpacity onPress={() => showSlideUp(item)}>
+                        <View style={styles.v6i}>
+                            <Fontisto name={"more-v-a"} size={18} color={"black"} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </>
+    );
+
     return (
         <SafeAreaView style={styles.sav}>
             <View style={styles.v1}>
                 <FlatList
                     data={files}
-                    renderItem={({ item, index }) => {
-                        return (
-                            <View style={styles.v2}>
-                                <View style={styles.v3}>
-                                    <TouchableOpacity onPress={()=>singleBlock(item, index)}>
-                                        <View style={styles.v3i}>
-                                            <View style={styles.v4}>
-                                                <Fontisto name={"applemusic"} size={60} color={"black"} />
-                                            </View>
-                                            <View style={styles.v5}>
-                                                <Text style={styles.t1}>{item.title.length > 35 ? item.title.substring(0, 35) + "..." : item.title}</Text>
-                                                <Text style={styles.t2}>{"Artist name"}</Text>
-                                                <Text style={styles.t3}>{"2:15"}</Text>
-                                            </View>
-                                        </View>
-                                    </TouchableOpacity>
-                                </View>
-                                <View style={styles.v6}>
-                                    <TouchableOpacity onPress={() => showSlideUp(item)}>
-                                        <View style={styles.v6i}>
-                                            <Fontisto name={"more-v-a"} size={18} color={"black"} />
-                                        </View>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                        );
-                    }}
+                    renderItem={({ item, index }) => (<SongBlock item={item} index={ index} />)}
                     keyExtractor={(item, index)=> index.toString()}
                 />
             </View>
